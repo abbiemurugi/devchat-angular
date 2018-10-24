67e6cf7396a9dcabd87c78f59a2f2800d40f9024
fatahrez <fertahrez@gmail.com>
2018-10-24 14:28:40 +0300

    update branch fatah

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
+// import { AngularFirestore} from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
+import {HttpModule } from '@angular/http';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyBX_PHQTnlZ3dNVV80Ud_3fW7J86JespSg",
+  authDomain: "devchat-d8c70.firebaseapp.com",
+  databaseURL: "https://devchat-d8c70.firebaseio.com",
+  storageBucket: "devchat-d8c70",
+  messagingSenderId: "137063297053"
+};
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
