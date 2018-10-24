f65f439994c602ca9e8da3613e56856474acf2eb
fatahrez <fertahrez@gmail.com>
2018-10-24 17:20:40 +0300

    merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
 import { AngularFireModule } from 'angularfire2';
@@ -24,20 +23,7 @@ import { AppComponent } from './app.component';
 // import { EmailComponent } from './email/email.component';
 // import { SignupComponent } from './signup/signup.component';
 // import { MembersComponent } from './members/members.component';
-=======
-import { AngularFireModule } from 'angularfire2';
-// import { AngularFirestore} from '@angular/fire/firestore';
-import { FormsModule } from '@angular/forms';
-import {HttpModule } from '@angular/http';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { EmailComponent } from './email/email.component';
-import { SignupComponent } from './signup/signup.component';
-import { MembersComponent } from './members/members.component';
->>>>>>> 67e6cf7396a9dcabd87c78f59a2f2800d40f9024
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBX_PHQTnlZ3dNVV80Ud_3fW7J86JespSg",
@@ -49,8 +35,6 @@ export const firebaseConfig = {
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-<<<<<<< HEAD
     UserComponent,
     RegisterComponent
     // LoginComponent,
@@ -66,19 +50,6 @@ export const firebaseConfig = {
     AngularFirestoreModule, 
     AngularFireAuthModule, 
     HttpClientModule
-=======
-    EmailComponent,
-    SignupComponent,
-    MembersComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AngularFireModule.initializeApp(firebaseConfig),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule
->>>>>>> 67e6cf7396a9dcabd87c78f59a2f2800d40f9024
   ],
   providers: [AuthService, UserService, UserResolver, AuthGuard],
   bootstrap: [AppComponent]
